5c171483a766e5294428602e5e25e8a52ac5b348
samuelxanda <samuelxanda752@gmail.com>
2025-04-07 17:00:58 +0200

    Add initial project files with navigation and scroll functionality

---
+++ main.js
@@ -0,0 +1,50 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+ScrollReveal({ 
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+ScrollReveal().reveal('.home-content,.heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, service-container,portfolio-box', { origin: 'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img,contact-social-media ', { origin: 'left'});
+ScrollReveal().reveal('.home-content p, about-content p', { origin: 'right'});
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Web Developer', 'UX/UI Designer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+})  
